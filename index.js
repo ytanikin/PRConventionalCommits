b86d5f8dbb50d5955699b8fa466c9e0b4320951d
Nic Westvold <nic.westvold@grafana.com>
2026-02-04 14:40:55 -0500

    chore: migrate to ESM with updated dependencies
    - Add "type": "module" to package.json for ESM support
    - Update @actions/core to ^3.0.0, @actions/github to ^9.0.0,
      conventional-commits-parser to ^6.2.1
    - Convert require/module.exports to import/export syntax
    - Update conventional-commits-parser usage to CommitParser class API
    - Replace Jest with Vitest for native ESM test support
    - Fix context.repo mock to use vi.spyOn getter for newer @actions/github

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { getInput, setFailed } = require('@actions/core');
-const { getOctokit, context } = require('@actions/github');
-const parser = require('conventional-commits-parser')
+import { getInput, setFailed } from '@actions/core';
+import { getOctokit, context } from '@actions/github';
+import { CommitParser } from 'conventional-commits-parser';
 
 
 /**
@@ -34,10 +34,11 @@ async function checkConventionalCommits() {
     }
 
     const pr = context.payload.pull_request;
-    const titleAst = parser.sync(pr.title.trimStart(), {
+    const parser = new CommitParser({
         headerPattern: /^(\w*)(?:\(([\w$.\-*/ ]*)\))?!?: (.*)$/,
         breakingHeaderPattern: /^(\w*)(?:\(([\w$.\-*/ ]*)\))?!: (.*)$/
     });
+    const titleAst = parser.parse(pr.title.trimStart());
     const cc = {
         type: titleAst.type ? titleAst.type : '',
         scope: titleAst.scope ? titleAst.scope : '',
@@ -179,7 +180,7 @@ function generateColor(str) {
 
 run().catch(err => setFailed(err.message));
 
-module.exports = {
+export {
     run,
     checkConventionalCommits,
     checkTicketNumber,
